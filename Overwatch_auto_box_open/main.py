8b9150929b2f054c83d7c1b3e3ab253a3deb8719
ZINO <wlsrkwkak@naver.com>
2020-09-21 13:09:17 +0900

    Jang

---
+++ Overwatch_auto_box_open/main.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+print('*' * 50 + "\n오버워치 전리품 상자깡 프로그램 v0.1\n " + '*' * 50)
+for i in range(0,1000):
+    print('스페이스 키를 {}번 눌렀습니다 - 두둥탁!\n'.format(i))
+    pyautogui.press  (' ')
+    time.sleep(1)
